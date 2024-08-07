3f0fdd3bd960eb783be389fc7d8f8f83adb52e5a
AndresR0910x <androd2000x@gmail.com>
2024-08-07 00:08:57 -0400

    Removed embedded repository Data-Structures

--- network_scanner.py
+++ network_scanner.py
@@ -6,12 +6,21 @@ def scan(ip):
     arp_request = scapy.ARP(pdst = ip) 
     broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout = 1)[0]
+    answered_list = scapy.srp(arp_request_broadcast, timeout = 1, verbose=False)[0]
     
+    clients_list = []
     for element in answered_list:
-        print(element[1].psrc)
-        print(element[1].hwsrc)
-        print("-------------------------------------------------------------------")
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
+        
+def print_result(result_list):
+    print("\nIP\t\t\tMAC address\n----------------------------------------")
+    for client in result_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+scan_result = scan("192.168.1.1/24")
+print_result(scan_result)
     
     #print(answered_list.summary())
     
@@ -24,5 +33,5 @@ def scan(ip):
 #Utilizamos la funcion 
 #Para poder identificar a todos los usuerios
 #en una subred se coloca /24
-scan("192.168.1.1/24")
+
 
